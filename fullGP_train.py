e431968cfb56e59fd9e873cd8f059be0a3776334
Sanket Salunkhe <sanketsalunkhe12@gmail.com>
2025-06-26 15:59:12 -0600

    hpc 30k dataset

--- fullGP_train.py
+++ fullGP_train.py
@@ -12,6 +12,8 @@ from utils import generate_dataset
 from utils import load_yaml_config
 from utils.results import plot_result, save_params
 
+torch.cuda.empty_cache()
+
 # local GP Model
 class ExactGPModel(gpytorch.models.ExactGP):
     def __init__(self, train_x, train_y, likelihood, kernel):
@@ -139,6 +141,9 @@ if __name__ == "__main__":
     kernel = gpytorch.kernels.RBFKernel(ard_num_dims=input_dim, lengthscale_constraint=gpytorch.constraints.Interval(0.01, 10.0))
     likelihood = gpytorch.likelihoods.GaussianLikelihood(noise_constraint=gpytorch.constraints.Interval(1e-4, 1.0))
     model = ExactGPModel(train_x, train_y, likelihood, kernel)
+
+    # model.double(False)
+    # likelihood.double(False)
     
     # train the model
     start_ = time.time()
--- results/scripts/run_49_expt.bash
+++ results/scripts/run_49_expt.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/bash
 
 NUM_RUN=10
-NUM_AGENTS=49
+NUM_AGENTS=9
 MAX_ATTEMPTS=3
 
 for i in $(seq 1 $NUM_RUN); do
     echo "Running pxpGP $i with agents: $NUM_AGENTS"
     
     # Retry until success
-    for j in ${MAX_ATTEMPTS}; do
-        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=12345 pxpGP_train.py
+    for j in $(seq 1 $MAX_ATTEMPTS); do
+        export MASTER_PORT=$((12000 + RANDOM % 10000))
+        surn torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=$MASTER_PORT pxpGP_train.py
         if [ $? -eq 0 ]; then
             echo "Run $i completed successfully"
             break
@@ -24,8 +25,9 @@ for i in $(seq 1 $NUM_RUN); do
     echo "Running gapxGP $i with agents: $NUM_AGENTS"
     
     # Retry until success
-    for j in ${MAX_ATTEMPTS}; do
-        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=12345 gapxGP_train.py
+    for j in $(seq 1 $MAX_ATTEMPTS); do
+        export MASTER_PORT=$((12000 + RANDOM % 10000))
+        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=$MASTER_PORT gapxGP_train.py
         if [ $? -eq 0 ]; then
             echo "Run $i completed successfully"
             break
@@ -40,8 +42,9 @@ for i in $(seq 1 $NUM_RUN); do
     echo "Running apxGP $i with agents: $NUM_AGENTS"
     
     # Retry until success
-    for j in ${MAX_ATTEMPTS}; do
-        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=12345 apxGP_train.py
+    for j in $(seq 1 $MAX_ATTEMPTS); do
+        export MASTER_PORT=$((12000 + RANDOM % 10000))
+        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=$MASTER_PORT apxGP_train.py
         if [ $? -eq 0 ]; then
             echo "Run $i completed successfully"
             break
@@ -56,8 +59,9 @@ for i in $(seq 1 $NUM_RUN); do
     echo "Running cGP $i with agents: $NUM_AGENTS"
     
     # Retry until success
-    for j in ${MAX_ATTEMPTS}; do
-        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=12345 cGP_train.py
+    for j in $(seq 1 $MAX_ATTEMPTS); do
+        export MASTER_PORT=$((12000 + RANDOM % 10000))
+        torchrun --nproc_per_node=$NUM_AGENTS --master_addr=localhost --master_port=$MASTER_PORT cGP_train.py
         if [ $? -eq 0 ]; then
             echo "Run $i completed successfully"
             break
